f62a9e8a417cefa7892d8355dcfcd36f2c354749
weoamathior <weoamathior@gmail.com>
2024-02-12 06:40:36 -0800

    flux from mono

---
+++ src/main/java/irish/bla/sec02/Lec09FluxFromMono.java
@@ -0,0 +1,15 @@
+package irish.bla.sec02;
+
+import irish.bla.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Lec09FluxFromMono {
+    public static void main(String[] args) {
+        Mono<String> mono = Mono.just("a");
+
+        Flux<String> flux = Flux.from(mono);
+
+        flux.subscribe(Util.onNext());
+    }
+}
