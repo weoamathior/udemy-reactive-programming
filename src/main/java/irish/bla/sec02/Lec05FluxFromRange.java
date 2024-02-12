2ff42cbd5472ce7beca78fc9f35215de0d77670c
weoamathior <weoamathior@gmail.com>
2024-02-12 06:11:56 -0800

    log statements

--- src/main/java/irish/bla/sec02/Lec05FluxFromRange.java
+++ src/main/java/irish/bla/sec02/Lec05FluxFromRange.java
@@ -7,7 +7,9 @@ public class Lec05FluxFromRange {
     public static void main(String[] args) {
 
         Flux.range(1,10)
+                .log()
                 .map(i -> Util.faker().name().fullName())
+                .log()
                 .subscribe(Util.onNext());
     }
 }
