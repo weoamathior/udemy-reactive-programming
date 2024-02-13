2db98b644fe9842345ead3623c876d2599b10437
weoamathior <weoamathior@gmail.com>
2024-02-12 16:55:23 -0800

    default subscriber

---
+++ src/main/java/irish/bla/util/DefaultSubscriber.java
@@ -0,0 +1,42 @@
+package irish.bla.util;
+
+import lombok.NoArgsConstructor;
+import org.reactivestreams.Subscriber;
+import org.reactivestreams.Subscription;
+@NoArgsConstructor
+public class DefaultSubscriber implements Subscriber<Object> {
+    private Subscription subscription;
+    private String name = "";
+
+    public DefaultSubscriber(String n) {this.name = n;}
+
+    @Override
+    public void onSubscribe(Subscription s) {
+        this.subscription = s;
+        s.request(Long.MAX_VALUE);
+    }
+
+    @Override
+    public void onNext(Object o) {
+        System.out.println(this.name + " received: " + o);
+
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        System.err.println(this.name + " Error: "  + t.getMessage());
+
+    }
+
+    @Override
+    public void onComplete() {
+        System.out.println(this.name + " completed");
+    }
+
+    public static Subscriber<Object> newInstance() {
+        return new DefaultSubscriber();
+    }
+    public static Subscriber<Object> newInstance(String name) {
+        return new DefaultSubscriber(name);
+    }
+}
