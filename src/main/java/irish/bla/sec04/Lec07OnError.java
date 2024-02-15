716031a89ae72601ae31ee2f9284a05cc1c04fb8
weoamathior <weoamathior@gmail.com>
2024-02-15 05:42:31 -0800

    onErrorResume, onErrorReturn, and onErrorContinue

---
+++ src/main/java/irish/bla/sec04/Lec07OnError.java
@@ -0,0 +1,28 @@
+package irish.bla.sec04;
+
+import irish.bla.util.DefaultSubscriber;
+import irish.bla.util.Util;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class Lec07OnError {
+
+    public static void main(String[] args) {
+
+        Flux.range(1,10)
+                .log()
+                .map(i -> 10 / (5 -i))
+//                .onErrorReturn(-1)
+//                .onErrorResume(err -> fallback())
+                // like a dlq - provides the error and the problematic object
+                .onErrorContinue((err,obj) -> {
+                    
+                })
+                .subscribe(DefaultSubscriber.newInstance());
+
+    }
+
+    private static Mono<Integer> fallback() {
+        return Mono.fromSupplier(() -> Util.faker().random().nextInt(100,200));
+    }
+}
