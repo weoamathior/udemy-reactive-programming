f28d211bc012f08059230a3088a957ed4a53eeb4
weoamathior <weoamathior@gmail.com>
2024-02-16 04:29:27 -0800

    default if empty

---
+++ src/main/java/irish/bla/sec04/Lec08DefaultIfEmpty.java
@@ -0,0 +1,21 @@
+package irish.bla.sec04;
+
+import irish.bla.util.DefaultSubscriber;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class Lec08DefaultIfEmpty {
+    public static void main(String[] args) {
+       getOrderNumbers()
+               .filter(i -> i> 10) // nothing will match
+               .defaultIfEmpty(-100)
+               .subscribe(DefaultSubscriber.newInstance());
+
+    }
+    private static Flux<Integer> getOrderNumbers() {
+
+        return Flux.range(1,10);
+
+    }
+}
