2613069e4564b9a0b8e17b0c2310e59fbbde0ab4
weoamathior <weoamathior@gmail.com>
2024-02-27 04:22:24 -0800

    capture dropped values on overflow

--- src/main/java/irish/bla/sec07/Lec02Drop.java
+++ src/main/java/irish/bla/sec07/Lec02Drop.java
@@ -6,12 +6,19 @@ import reactor.core.publisher.Flux;
 import reactor.core.scheduler.Schedulers;
 import reactor.util.concurrent.Queues;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Lec02Drop {
     public static void main(String[] args) {
         // From Queues reactor class.  Fiddling with the internal queue size
         System.setProperty("reactor.bufferSize.small", "16");
+
+        List<Object> list = new ArrayList<>();
+
+
         Flux.create(fluxSink -> {
-            for (int i = 1; i < 501; i++) {
+            for (int i = 1; i < 201; i++) {
                 fluxSink.next(i);
                 System.out.println("pushed " + i);
                 Util.sleepMillis(1);
@@ -19,7 +26,7 @@ public class Lec02Drop {
             }
             fluxSink.complete();
         })
-                .onBackpressureDrop()
+                .onBackpressureDrop(list::add)
                 .publishOn(Schedulers.boundedElastic())
                 .doOnNext(i -> {
                     Util.sleepMillis(10);
@@ -27,6 +34,7 @@ public class Lec02Drop {
                 })
                 .subscribe(DefaultSubscriber.newInstance());
 
-        Util.sleepSeconds(60);
+        Util.sleepSeconds(10);
+        System.out.println(list);
     }
 }
